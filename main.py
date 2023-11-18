96e1c7f95f6b1a3b102fad5adaf2c44328eb3354
ranlom01 <61160011+ranlom01@users.noreply.github.com>
2023-11-18 17:46:03 +0100

    Gradio interface

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import pandas as pd
 
 coqa = pd.read_json('http://downloads.cs.stanford.edu/nlp/data/coqa/coqa-train-v1.0.json')
 
-def display_header():
-    return coqa.head().to_string()
+def random_response(message, history=None):
+    return message
 
-iface = gr.Interface(
-    fn=display_header,
-    inputs=None,
-    outputs=gr.Textbox(),
-    title="COQA Dataset Header",
-    description="Display the header of the COQA dataset."
-)
+iface = gr.ChatInterface(fn=random_response,
+    title="Question answering with Distillbert",
+    description="This bot gives an answer for your questions, it doesn't have memory",
+    )
+
 iface.launch(server_name="0.0.0.0")
