b11bf46c96e115808cbfdf2a227fcbcbce15775a
ranlom01 <61160011+ranlom01@users.noreply.github.com>
2023-11-18 17:11:52 +0100

    Dockeres működés gradio szerverrel

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import gradio as gr
 import pandas as pd
 
-#Read the CoQA dataset
 coqa = pd.read_json('http://downloads.cs.stanford.edu/nlp/data/coqa/coqa-train-v1.0.json')
 
 def display_header():
     return coqa.head().to_string()
 
-#Create a Gradio interface
-demo = gr.Interface(fn=display_header, inputs=None, outputs="text", title="CoQA DataFrame Header")
-
-demo.launch()
+iface = gr.Interface(
+    fn=display_header,
+    inputs=None,
+    outputs=gr.Textbox(),
+    title="COQA Dataset Header",
+    description="Display the header of the COQA dataset."
+)
+iface.launch(server_name="0.0.0.0")
