2fb23e6d3e30ffac59e40751675d5c11a395f927
ranlom01 <61160011+ranlom01@users.noreply.github.com>
2023-11-19 15:51:19 +0100

    DistillBert

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import gradio as gr
 import pandas as pd
-
+from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
 import torch
-from transformers import BertForQuestionAnswering
-from transformers import BertTokenizer
 
-model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-
-tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased',return_token_type_ids = True)
+model = DistilBertForQuestionAnswering.from_pretrained('distilbert-base-uncased-distilled-squad')
 
 #Dataset
-question = '''What is Machine Learning?'''
-
 paragraph = ''' Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to progressively improve their performance
                 on a specific task. Machine learning algorithms build a mathematical model of sample data, known as "training data", in order to make predictions or
                 decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in the applications of email filtering, detection
@@ -22,27 +17,44 @@ paragraph = ''' Machine learning (ML) is the scientific study of algorithms and 
 
 coqa = pd.read_json('http://downloads.cs.stanford.edu/nlp/data/coqa/coqa-train-v1.0.json')
 
+def ask_model(question,context):
+
+  #question = "Who has the highest cases?"
+
+  encoding = tokenizer.encode_plus(question, context)
 
 
-def ask_model(question, paragraph=paragraph):
-    encoding = tokenizer.encode_plus(text=question,text_pair=paragraph)
-    inputs = encoding['input_ids']  #Token embeddings
-    sentence_embedding = encoding['token_type_ids']  #Segment embeddings
-    tokens = tokenizer.convert_ids_to_tokens(inputs) #input tokens
-    start_scores, end_scores = model(input_ids=torch.tensor([inputs]), token_type_ids=torch.tensor([sentence_embedding]),return_dict=False )
-    start_index = torch.argmax(start_scores)
-    end_index = torch.argmax(end_scores)
-    answer = ' '.join(tokens[start_index:end_index+1])
-    corrected_answer = ''
+  input_ids, attention_mask = encoding["input_ids"], encoding["attention_mask"]
 
-    for word in answer.split():
+  start_scores = model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))["start_logits"]
+  end_scores = model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))["end_logits"]
 
-    #If it's a subword token
-      if word[0:2] == '##':
-          corrected_answer += word[2:]
-      else:
-          corrected_answer += ' ' + word
-    return corrected_answer
+  ans_tokens = input_ids[torch.argmax(start_scores) : torch.argmax(end_scores)+1]
+  answer_tokens = tokenizer.convert_ids_to_tokens(ans_tokens , skip_special_tokens=True)
+
+  answer_tokens_to_string = tokenizer.convert_tokens_to_string(answer_tokens)
+
+  return answer_tokens_to_string
+
+# def ask_model(question, paragraph=paragraph):
+#     encoding = tokenizer.encode_plus(text=question,text_pair=paragraph)
+#     inputs = encoding['input_ids']  #Token embeddings
+#     sentence_embedding = encoding['token_type_ids']  #Segment embeddings
+#     tokens = tokenizer.convert_ids_to_tokens(inputs) #input tokens
+#     start_scores, end_scores = model(input_ids=torch.tensor([inputs]), token_type_ids=torch.tensor([sentence_embedding]),return_dict=False )
+#     start_index = torch.argmax(start_scores)
+#     end_index = torch.argmax(end_scores)
+#     answer = ' '.join(tokens[start_index:end_index+1])
+#     corrected_answer = ''
+
+#     for word in answer.split():
+
+#     #If it's a subword token
+#       if word[0:2] == '##':
+#           corrected_answer += word[2:]
+#       else:
+#           corrected_answer += ' ' + word
+#     return corrected_answer
 
 
 
